a5b068e9a01571f2259cefb1f05d27f917461eb6
JiHyeok Kim <jkl410907@gmail.com>
2023-02-14 12:06:56 +0900

    Feat: BaseController

---
+++ src/main/java/com/example/webserver/controller/BaseController.java
@@ -0,0 +1,21 @@
+package com.example.webserver.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BaseController {
+
+    @RequestMapping(value="/callback", method = RequestMethod.GET)
+    public String getCallback(@RequestParam String code) {
+        return code;
+    }
+
+    @RequestMapping(value="/health", method = RequestMethod.GET)
+    public String healthCheck() {
+        return "{\"status\":\"UP\"}";
+    }
+
+}
